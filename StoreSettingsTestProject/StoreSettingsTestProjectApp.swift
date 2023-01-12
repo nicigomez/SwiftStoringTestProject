2439e6acf43f23b1e0387dd02c2e2f24abf38346
nicigomez <nicolas_lerch@gmx.de>
2023-01-12 15:45:37 +0100

    Initial Commit

---
+++ StoreSettingsTestProject/StoreSettingsTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreSettingsTestProjectApp.swift
+//  StoreSettingsTestProject
+//
+//  Created by Nicolas Lerch on 12.01.23.
+//
+
+import SwiftUI
+
+@main
+struct StoreSettingsTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
